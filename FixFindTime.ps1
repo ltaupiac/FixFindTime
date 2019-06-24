47e37def848746fd784fb96ebaee2c6e0d40a950
ltaupiac <ltaupiac@lfdj.com>
2019-06-24 14:53:09 +0200

    add header

---
+++ FixFindTime.ps1
@@ -0,0 +1,16 @@
+<#
+.SYNOPSIS
+Add findtime.microsoft.com to trusted zone to avoid pb with proxy
+
+.DESCRIPTION
+Add findtime.microsoft.com to trusted zone to avoid pb with proxy
+
+.FUNCTIONALITY
+On-demand
+
+.NOTES
+Context:            LocalSystem
+Version:            1.0.0.0 - Initial release
+Last modified: 		2019/06/24 14:51:55
+#>
+
